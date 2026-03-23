03077e56ce629c674f7da6622bd24c7c047cd242
rmysliwczyk <31518644+rmysliwczyk@users.noreply.github.com>
2026-03-23 15:32:22 +0000

    Removed referencing by non-unique 'title' column

--- app/db_operations/book.py
+++ app/db_operations/book.py
@@ -20,33 +20,26 @@ def create_book(session: Session, book: BookCreate) -> Book:
 
 
 def read_book(
-    session: Session, title: str | None = None, id: uuid.UUID | None = None
+    session: Session, id: uuid.UUID | None = None
 ) -> Book:
     book = None
 
-    if title and id:
-        raise ValueError("Provide title or id, not both.")
-    elif title:
-        book = session.exec(select(Book).where(Book.title == title)).first()
-        if not book:
-            raise BookNotFound(f"Book {title} not found.")
-    elif id:
-        book = session.exec(select(Book).where(Book.id == id)).first()
-        if not book:
-            raise BookNotFound(f"Book with id {id} not found.")
-    else:
-        raise ValueError("Provide title or id.")
-
+    if not id:
+        raise ValueError("Provide book id.")
+    
+    book = session.exec(select(Book).where(Book.id == id)).first()
+    if not book:
+        raise BookNotFound(f"Book with id {id} not found.")
+    
     return book
 
 
 def update_book(
     session: Session,
     data: BookUpdate,
-    title: str | None = None,
     id: uuid.UUID | None = None,
 ) -> Book:
-    book = read_book(session, title=title, id=id)
+    book = read_book(session, id=id)
 
     book.sqlmodel_update(data.model_dump(exclude_unset=True))
     session.add(book)
@@ -55,8 +48,8 @@ def update_book(
 
     return book
 
-def delete_book(session: Session, title: str | None = None, id: uuid.UUID | None = None):
-    book_for_deletion = read_book(session, title=title, id=id)
+def delete_book(session: Session, id: uuid.UUID | None = None):
+    book_for_deletion = read_book(session, id=id)
     session.delete(book_for_deletion)
     session.commit()
 
