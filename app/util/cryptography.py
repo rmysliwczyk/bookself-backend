9f76a151f9451a21c97cd864142b9828b928fbee
Rafał Myśliwczyk <mysliwczykrafal@gmail.com>
2026-03-24 19:40:47 +0100

    Added TC: password hashing and verification

--- app/util/cryptography.py
+++ app/util/cryptography.py
@@ -1,3 +1,11 @@
+from pwdlib import PasswordHash
+
+password_hasher = PasswordHash.recommended()
+
+
 def hash_password(password: str) -> str:
-    hashed_password = password + "_HASHED"
-    return hashed_password
+    return password_hasher.hash(password)
+
+
+def verify_password(password: str, hashed_password: str) -> bool:
+    return password_hasher.verify(password, hashed_password)
