9bee090ed82970d0c5a23871dfec8eb04a5d0ea7
Rafał Myśliwczyk <mysliwczykrafal@gmail.com>
2026-06-02 20:48:27 +0200

    Added missing algorithm

--- app/settings.py
+++ app/settings.py
@@ -4,8 +4,8 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     model_config = {"env_file": ".env"}
     database_url: str = Field(default="sqlite:///test.db")
-    admin_username: str = Field(default="CHANGEME")
-    admin_password: str = Field(default="CHANGEME")
-    algorithm: str = Field(default="CHANGEME")
+    admin_username: str = Field(default="ADMIN")
+    admin_password: str = Field(default="ADMIN")
+    algorithm: str = Field(default="HS256")
     secret_key: str = Field(default="CHANGEME")
 
