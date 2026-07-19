c8a906dec2dfe7aca6be9f1f895091723cae069f
Rafał Myśliwczyk <mysliwczykrafal@gmail.com>
2026-07-19 16:04:06 +0200

    Fixed url

--- app/routes/books.py
+++ app/routes/books.py
@@ -40,10 +40,10 @@ def create(session: SessionDep, current_user: Annotated[User, Depends(get_curren
         case _:
             raise RequestValidationError("Invalid image format")
 
-    filepath = f"/books/{settings.media_base_url}{cover_picture.filename if cover_picture.filename else 'unkown'}_{str(data.user_id)}{extension}"
+    filepath = f"{settings.media_base_url}{cover_picture.filename if cover_picture.filename else 'unkown'}_{str(data.user_id)}{extension}"
     with open(filepath, "wb") as f:
         f.write(cover_picture.file.read())
-    file_url = f"{settings.api_url}{filepath}"
+    file_url = f"{settings.api_url}books/{filepath}"
     new_book = create_book(session, data, cover_photo_url=file_url)
     return new_book
 
